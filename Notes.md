ff4b46fd59f17ff8b75ac2d971687b3fbf26e570
Andrew <a.oppman@gmail.com>
2023-07-15 17:53:13 -0400

    updated main.js

---
+++ Notes.md
@@ -0,0 +1,13 @@
+Creating elements, adding them and editing the text within them
+
+append() - can append mutliple elements as well as string to to document.
+vs appendChild() - which can only append a single element to the document.
+
+"createElement()" only creates the element but does not add it to the DOM - the newly created element then needs to be *appended to the DOM*
+.. creating an element in JS does not automatically add it to the HTML
+
+to set text to an element you can use ".innerText" or ".textContent"
+
+
+
+
--- main.js
+++ main.js
@@ -23,3 +23,43 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+const errorModal = document.getElementById("modal");
+
+//error.className = "hidden"
+
+
+
+//hearts.addEventListener('click', mimicServerCall)
+
+const hearts = document.querySelectorAll(".like-glyph")
+  for(let i = 0; i < hearts.length; i++) {
+    hearts[i].addEventListener('click', function() {
+      mimicServerCall()
+      .then(function(response) {
+        if (hearts[i].classList.contains('activated-heart')) {
+          hearts[i].classList.remove('activated-heart')
+          hearts[i].textContent = EMPTY_HEART;
+        }
+        else {
+          hearts[i].classList.add('activated-heart')
+          hearts[i].textContent = FULL_HEART;
+        }
+      })
+      .catch(function(error) {
+        errorModal.classList.remove('hidden');
+        const errorMessage = errorModal.querySelector('p');
+        errorMessage.textContent = error;
+        setTimeout(function() {
+          errorModal.classList.add('hidden');
+        }, 3000);
+        })
+      })
+  };
+
+
+
+
+
+
+
